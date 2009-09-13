63a588f6d58871d578b1a1636d3b7c4d635a9911
Ali Polatel <alip@exherbo.org>
2009-09-13 17:09:09 +0300

    Start adding tests for tracing 32bit bins on 64bit
    Add 32 bit tests for chmod

--- tests/progtests/t01-chmod.bash
+++ tests/progtests/t01-chmod.bash
@@ -16,6 +16,23 @@ if [[ "${perms}" != '-rw-r--r--' ]]; then
 fi
 end_test
 
+if $check_32bit; then
+    start_test "t01-chmod-deny-32bit"
+    if [[ -z $linux32 ]]; then
+        say skip "linux32 not found skipping test"
+    else
+        sydbox -- $linux32 ./t01_chmod
+        if [[ 0 == $? ]]; then
+            die "failed to deny chmod"
+        fi
+        perms=$(ls -l arnold.layne | cut -d' ' -f1)
+        if [[ "${perms}" != '-rw-r--r--' ]]; then
+            die "permissions changed, failed to deny chmod"
+        fi
+    fi
+    end_test
+fi
+
 start_test "t01-chmod-write"
 SYDBOX_WRITE="${cwd}" sydbox -- ./t01_chmod
 if [[ 0 != $? ]]; then
@@ -26,6 +43,23 @@ if [[ "${perms}" != '----------' ]]; then
     die "write didn't allow access"
 fi
 end_test
+
+if $check_32bit; then
+    start_test "t01-chmod-write-32bit"
+    if [[ -z $linux32 ]]; then
+        say skip "linux32 not found skipping test"
+    else
+        SYDBOX_WRITE="${cwd}" sydbox -- $linux32 ./t01_chmod
+        if [[ 0 != $? ]]; then
+            die "failed to allow chmod"
+        fi
+        perms=$(ls -l arnold.layne | cut -d' ' -f1)
+        if [[ "${perms}" != '----------' ]]; then
+            die "write didn't allow access"
+        fi
+    fi
+    end_test
+fi
 
 # Tests dealing with too long paths
 tmpfile="$(mkstemp_long)"
@@ -41,6 +75,19 @@ if [[ 0 == $? ]]; then
 fi
 end_test
 
+if $check_32bit; then
+    start_test "t01-chmod-deny-toolong-32bit"
+    if [[ -z $linux32 ]]; then
+        say skip "linux32 not found skipping test"
+    else
+        sydbox -- $linux32 ./t01_chmod_toolong "$long_dir" "$tmpfile"
+        if [[ 0 == $? ]]; then
+            die "failed to deny chmod"
+        fi
+    fi
+    end_test
+fi
+
 start_test "t01-chmod-allow-toolong"
 SYDBOX_WRITE="$cwd"/$long_dir sydbox -- ./t01_chmod_toolong "$long_dir" "$tmpfile"
 if [[ 0 != $? ]]; then
@@ -54,3 +101,23 @@ elif [[ "$perms" != 0 ]]; then
     die "write didn't allow access"
 fi
 end_test
+
+if $check_32bit; then
+    start_test "t01-chmod-allow-toolong-32bit"
+    if [[ -z $linux32 ]]; then
+        say skip "linux32 not found skipping test"
+    else
+        SYDBOX_WRITE="$cwd"/$long_dir sydbox -- $linux32 ./t01_chmod_toolong "$long_dir" "$tmpfile"
+        if [[ 0 != $? ]]; then
+            die "failed to allow chmod"
+        fi
+        perms=$(perm_long "$tmpfile")
+        if [[ -z "$perms" ]]; then
+            say skip "failed to get permissions of the file, skipping test"
+            exit 0
+        elif [[ "$perms" != 0 ]]; then
+            die "write didn't allow access"
+        fi
+    fi
+    end_test
+fi
