ef462ec071211ce18144cf7c3ebddc0b1849bb52
Ali Polatel <alip@exherbo.org>
2009-12-11 00:40:21 +0200

    convert check tests to gtest tests (part 1)

--- tests/unit/test-trace.c
+++ tests/unit/test-trace.c
@@ -25,8 +25,11 @@
 #include "config.h"
 #endif // HAVE_CONFIG_H
 
+#include <errno.h>
+#include <fcntl.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -119,12 +122,158 @@ static void test2(void)
     }
 }
 
+static void test7(void)
+{
+    int ret, status;
+    pid_t pid;
+
+    pid = fork();
+    if (0 > pid) {
+        g_printerr("fork() failed: %s\n", g_strerror(errno));
+        g_assert(FALSE);
+    }
+    else if (0 == pid) { // child
+        if (0 > trace_me())
+            g_printerr("trace_me() failed: %s\n", g_strerror(errno));
+        kill(getpid(), SIGSTOP);
+        kill(getpid(), SIGINT);
+    }
+    else { // parent
+        waitpid(pid, &status, 0);
+
+        if (!WIFSTOPPED(status)) {
+            g_printerr("child %i didn't stop by sending itself SIGSTOP\n", pid);
+            g_assert(FALSE);
+        }
+        if (0 > trace_setup(pid)) {
+            g_printerr("failed to set tracing options: %s", g_strerror(errno));
+            g_assert(FALSE);
+        }
+
+        /* Resume the child, it will receive a SIGINT */
+        if (0 > trace_cont(pid)) {
+            g_printerr("trace_cont() failed: %s", g_strerror(errno));
+            g_assert(FALSE);
+        }
+        waitpid(pid, &status, 0);
+
+        /* Check the event */
+        ret = trace_event(status);
+        if (E_GENUINE != ret) {
+            g_printerr("Expected E_GENUINE got %d", ret);
+            g_assert(FALSE);
+        }
+
+        trace_kill(pid);
+    }
+}
+
+static void test8(void)
+{
+    int ret, status;
+    pid_t pid;
+
+    pid = fork();
+    if (0 > pid) {
+        g_printerr("fork() failed: %s\n", g_strerror(errno));
+        g_assert(FALSE);
+    }
+    else if (0 == pid) { // child
+        if (0 > trace_me())
+            g_printerr("trace_me() failed: %s\n", g_strerror(errno));
+        kill(getpid(), SIGSTOP);
+        exit(EXIT_SUCCESS);
+    }
+    else { // parent
+        waitpid(pid, &status, 0);
+
+        if (!WIFSTOPPED(status)) {
+            g_printerr("child %i didn't stop by sending itself SIGSTOP\n", pid);
+            g_assert(FALSE);
+        }
+        if (0 > trace_setup(pid)) {
+            g_printerr("failed to set tracing options: %s", g_strerror(errno));
+            g_assert(FALSE);
+        }
+
+        /* Resume the child, it will exit normally */
+        if (0 > trace_cont(pid)) {
+            g_printerr("trace_cont() failed: %s", g_strerror(errno));
+            g_assert(FALSE);
+        }
+        waitpid(pid, &status, 0);
+
+        /* Check the event */
+        ret = trace_event(status);
+        if (E_EXIT != ret) {
+            g_printerr("Expected E_EXIT got %d", ret);
+            g_assert(FALSE);
+        }
+
+        trace_kill(pid);
+    }
+}
+
+static void test9(void)
+{
+    int ret, status;
+    pid_t pid;
+
+    pid = fork();
+    if (0 > pid) {
+        g_printerr("fork() failed: %s\n", g_strerror(errno));
+        g_assert(FALSE);
+    }
+    else if (0 == pid) { // child
+        if (0 > trace_me())
+            g_printerr("trace_me() failed: %s\n", g_strerror(errno));
+        kill(getpid(), SIGSTOP);
+        pause();
+    }
+    else { // parent
+        waitpid(pid, &status, 0);
+
+        if (!WIFSTOPPED(status)) {
+            g_printerr("child %i didn't stop by sending itself SIGSTOP\n", pid);
+            g_assert(FALSE);
+        }
+        if (0 > trace_setup(pid)) {
+            g_printerr("failed to set tracing options: %s", g_strerror(errno));
+            g_assert(FALSE);
+        }
+
+        /* Resume the child and kill it with a signal */
+        if (0 > trace_cont(pid)) {
+            g_printerr("trace_cont() failed: %s", g_strerror(errno));
+            g_assert(FALSE);
+        }
+        kill(pid, SIGKILL);
+        waitpid(pid, &status, 0);
+
+        /* Check the event */
+        ret = trace_event(status);
+        if (E_EXIT_SIGNAL != ret) {
+            g_printerr("Expected E_EXIT_SIGNAL got %d", ret);
+            g_assert(FALSE);
+        }
+    }
+}
+
 int main(int argc, char **argv)
 {
     g_test_init(&argc, &argv, NULL);
 
     g_test_add_func("/trace/event/stop", test1);
     g_test_add_func("/trace/event/syscall", test2);
+/* TODO
+ *  g_test_add_func("/trace/event/fork", test3);
+ *  g_test_add_func("/trace/event/vfork", test4);
+ *  g_test_add_func("/trace/event/clone", test5);
+ *  g_test_add_func("/trace/event/execv", test6);
+ */
+    g_test_add_func("/trace/event/genuine", test7);
+    g_test_add_func("/trace/event/exit/normal", test8);
+    g_test_add_func("/trace/event/exit/signal", test9);
 
     return g_test_run();
 }
