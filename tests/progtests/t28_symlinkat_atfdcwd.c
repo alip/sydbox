3f7e88f4a1ddaae401f650c6dd588a624b2b7f0c
Ali Polatel <alip@exherbo.org>
2010-01-07 11:54:03 +0200

    progtests: fix compiler warnings

--- tests/progtests/t28_symlinkat_atfdcwd.c
+++ tests/progtests/t28_symlinkat_atfdcwd.c
@@ -9,6 +9,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(void) {
     if (0 > symlinkat("see.emily.play/gnome", AT_FDCWD, "jugband.blues"))
--- tests/progtests/t29_symlinkat.c
+++ tests/progtests/t29_symlinkat.c
@@ -11,6 +11,7 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(void) {
     DIR *dot = opendir(".");
